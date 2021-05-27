71d02fb1764d041e5d1e491d859e171e1a597f3f
zhangxu <zhangxu@91160.com>
2021-05-27 15:34:10 +0800

    增加字体

--- src/components/widget/UserIntro/index.tsx
+++ src/components/widget/UserIntro/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Date         : 2021-05-26 15:24:40
  * @LastEditors  : zhangxu
- * @LastEditTime : 2021-05-26 16:13:49
+ * @LastEditTime : 2021-05-27 15:24:12
  * @FilePath     : /shuyuxuan/shuyuxuan_blog_front/src/components/widget/UserIntro/index.tsx
  */
 import React from 'react'
@@ -10,7 +10,7 @@ import styles from './index.less'
 const UserIntro = () => {
   return (<div className={styles.userIntro}>
     <div className={styles.name}>张书语</div>
-    <div className={styles.subInfo}>一个自觉文艺的90后前端工程师</div>
+    <div className={styles.subInfo}>一个带点文艺范的90后前端工程师</div>
     <img className={styles.avatar} src="https://shuyuxuan-1254264552.cos.ap-chengdu.myqcloud.com/shuyuxuanRes/images/avatar01.jpg" alt="" />
   </div>)
 }
