7e61eac54e4b99fc299d36499683cb4eabf24d24
zhangxu <zhangxu@91160.com>
2021-07-28 10:13:04 +0800

    构建发布脚本

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 ###
  # @Date         : 2021-07-28 09:05:10
  # @LastEditors  : zhangxu
- # @LastEditTime : 2021-07-28 09:08:10
+ # @LastEditTime : 2021-07-28 10:09:46
  # @FilePath     : /shuyuxuan_blog_front/build.sh
 ###
+yarn build
 docker build -t shuyuxuan_blog_front .
 docker tag shuyuxuan_blog_front:latest registry.cn-hangzhou.aliyuncs.com/zhangxu0413/shuyuyuxuan_blog_front:$1
-docker push shuyuxuan_blog_front:latest registry.cn-hangzhou.aliyuncs.com/zhangxu0413/shuyuyuxuan_blog_front:$1
+docker push registry.cn-hangzhou.aliyuncs.com/zhangxu0413/shuyuyuxuan_blog_front:$1
